8e69d84427a35511e2f30e7159f7afe825665550
Stefan Husch <s.husch@qutic.com>
2010-09-15 09:24:10 +0200

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,84 @@
+# Debian/nexenta specific build module
+#
+# build::install { 'top':
+#   download => 'http://www.unixtop.org/dist/top-3.7.tar.gz',
+#   creates  => '/url/local/bin/top',
+# }
+
+define build::install ($download, $creates, $pkg_folder='', $pkg_format="tar", $pkg_extension="", $buildoptions="", $extractorcmd="", $rm_build_folder=true) {
+  
+  if defined( Package['build-essential'] ) {
+    debug("Package build-essential already installed")
+  } else {
+    package { 'build-essential': ensure => installed }
+  }
+  
+  $cwd    = "/usr/local/src"
+  
+  $unzip  = "/usr/bin/unzip"
+  $tar    = "/usr/sbin/tar"
+  $bunzip = "/usr/bin/bunzip2"
+  $gunzip = "/usr/bin/gunzip"
+  
+  $filename = basename($download)
+  
+  $extension = $pkg_format ? {
+    zip     => ".zip",
+    bzip    => ".tar.bz2",
+    tar     => ".tar.gz",
+    default => $pkg_extension,
+  }
+  
+  $foldername = $pkg_folder ? {
+    ''      => gsub($filename, $extension, ""),
+    default => $pkg_folder,
+  }
+  
+  $extractor = $pkg_format ? {
+    zip     => "$unzip -q -d $cwd $cwd/$filename",
+    bzip    => "$bunzip -c $cwd/$filename | $tar -xf -",
+    tar     => "$gunzip < $cwd/$filename | $tar -xf -",
+    default => $extractorcmd,
+  }
+  
+  exec { "download-$name":
+    cwd     => "$cwd",
+    command => "/usr/bin/wget -q $download",
+    timeout => 120, # 2 minutes
+    unless  => "test -f $creates",
+  }
+  
+  exec { "extract-$name":
+    cwd     => "$cwd",
+    command => "$extractor",
+    timeout => 120, # 2 minutes
+    require => Exec["download-$name"],
+  }
+  
+  exec { "config-$name":
+    cwd     => "$cwd/$foldername",
+    command => "$cwd/$foldername/configure $buildoptions",
+    timeout => 120, # 2 minutes
+    require => Exec["extract-$name"],
+  }
+  
+  exec { "make-install-$name":
+    cwd     => "$cwd/$foldername",
+    command => "/usr/bin/make && /usr/bin/make install",
+    timeout => 600, # 10 minutes
+    require => Exec["config-$name"],
+  }
+  
+  # remove build folder
+  case $rm_build_folder {
+    true: {
+      notice("remove build folder")
+      exec { "remove-$name-build-folder":
+        cwd     => "$cwd",
+        command => "/usr/bin/rm -rf $cwd/$foldername",
+        require => Exec["make-install-$name"],
+      } # exec
+    } # true
+  } # case
+  
+}
