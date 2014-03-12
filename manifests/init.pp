af0da6acb26d2097b3168a2cdd0e637046d9c6a4
toimtoimtoim <desinformatsioon@gmail.com>
2014-03-12 22:31:54 +0200

    Added ability to build packages from source when source is stored locally or within puppet module files. Added Modulefile to ease usage with module dependencies Refactored code to meet Puppet style guide

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,106 +1,108 @@
 # Debian/nexenta specific build module
 #
 # build::install { 'top':
-#   download => 'http://www.unixtop.org/dist/top-3.7.tar.gz',
+#   file => 'http://www.unixtop.org/dist/top-3.7.tar.gz',
 #   creates  => '/usr/local/bin/top',
 # }
 
 define build::install (
-  $download,
+  $file,
   $creates,
-  $pkg_folder='',
-  $pkg_format="tar",
-  $pkg_extension="",
-  $buildoptions="",
-  $extractorcmd="",
-  $make_cmd="",
-  $rm_build_folder=true) {
-  
-  build::requires { "$name-requires-build-essential":  package => 'build-essential' }
-  
+  $pkg_folder         = '',
+  $pkg_format         = 'tar',
+  $pkg_extension      = '',
+  $build_options      = '',
+  $extractor_cmd      = '',
+  $extract_options    = '',
+  $make_cmd           = '',
+  $src_cwd            = '/usr/local/src',
+  $rm_build_folder    = true
+  ) {
+
+  if $file == undef {
+    fail('parameter $file not set')
+  }
+
+  $filename = basename($file)
+  $extratable_file = "${src_cwd}/${filename}"
+
   Exec {
-    unless => "$test -f $creates",
+    unless => "test -f ${creates}",
+    path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ]
   }
   
-  $cwd    = "/usr/local/src"
-  
-  $test   = "/usr/bin/test"
-  $unzip  = "/usr/bin/unzip"
-  $tar    = "/usr/sbin/tar"
-  $bunzip = "/usr/bin/bunzip2"
-  $gunzip = "/usr/bin/gunzip"
-  
-  $filename = basename($download)
-  
   $extension = $pkg_format ? {
-    zip     => ".zip",
-    bzip    => ".tar.bz2",
-    tar     => ".tar.gz",
-    default => $pkg_extension,
+    'zip'     => ".zip",
+    'bzip'    => ".tar.bz2",
+    'tar'     => ".tar.gz",
+    default   => $pkg_extension,
   }
   
   $foldername = $pkg_folder ? {
     ''      => gsub($filename, $extension, ""),
     default => $pkg_folder,
   }
-  
-  $extractor = $pkg_format ? {
-    zip     => "$unzip -q -d $cwd $cwd/$filename",
-    bzip    => "$bunzip -c $cwd/$filename | $tar -xf -",
-    tar     => "$gunzip < $cwd/$filename | $tar -xf -",
-    default => $extractorcmd,
+
+  ensure_resource('package', 'build-essential', {'ensure' => 'present' })
+  ensure_resource('package', 'wget', {'ensure' => 'present' })
+
+  if $file =~ /^http/ {
+    notice("downloading ${file}")
+    exec { "download-${name}":
+      cwd     => $src_cwd,
+      command => "wget -q ${file}",
+      timeout => 120, # 2 minutes
+      before  => Exec["extract-${name}"],
+    }
+  } else {
+    notice("copying local file ${file}")
+    file { "${extratable_file}":
+      ensure  => present,
+      source  => $file,
+      before  => Exec["extract-${name}"],
+    }
   }
 
+  $extractor = $pkg_format ? {
+    'zip'     => "unzip -q ${extract_options} -d ${src_cwd} ${extratable_file}",
+    'bzip'    => "bunzip2 -c ${extratable_file} | tar ${extract_options} -xf -",
+    'tar'     => "gunzip < ${extratable_file} | tar ${extract_options} -xf -",
+    default   => $extractor_cmd,
+  }
+
+  exec { "extract-${name}":
+    cwd     => $src_cwd,
+    command => $extractor,
+    timeout => 120, # 2 minutes,
+  }
+
+  exec { "config-${name}":
+    cwd     => "${src_cwd}/${foldername}",
+    command => "${src_cwd}/${foldername}/configure ${buildoptions}",
+    timeout => 120, # 2 minutes
+    require => Exec["extract-${name}"],
+  }
+  
   $make = $make_cmd ? {
-    '' => '/usr/bin/make && /usr/bin/make install',
+    ''      => 'make && make install',
+    verbose => true,
     default => $make_cmd,
   }
-  
-  exec { "download-$name":
-    cwd     => "$cwd",
-    command => "/usr/bin/wget -q $download",
-    timeout => 120, # 2 minutes
-  }
-  
-  exec { "extract-$name":
-    cwd     => "$cwd",
-    command => "$extractor",
-    timeout => 120, # 2 minutes
-    require => Exec["download-$name"],
-  }
-  
-  exec { "config-$name":
-    cwd     => "$cwd/$foldername",
-    command => "$cwd/$foldername/configure $buildoptions",
-    timeout => 120, # 2 minutes
-    require => Exec["extract-$name"],
-  }
-  
-  exec { "make-install-$name":
-    cwd     => "$cwd/$foldername",
-    command => "$make",
-    timeout => 600, # 10 minutes
-    require => Exec["config-$name"],
-  }
-  
-  # remove build folder
-  case $rm_build_folder {
-    true: {
-      notice("remove build folder")
-      exec { "remove-$name-build-folder":
-        cwd     => "$cwd",
-        command => "/usr/bin/rm -rf $cwd/$foldername",
-        require => Exec["make-install-$name"],
-      } # exec
-    } # true
-  } # case
-  
-}
 
-define build::requires ( $ensure='installed', $package ) {
-  if defined( Package[$package] ) {
-    debug("$package already installed")
-  } else {
-    package { $package: ensure => $ensure }
+  exec { "make-install-${name}":
+    cwd     => "${src_cwd}/${foldername}",
+    command => $make,
+    timeout => 600, # 10 minutes
+    require => Exec["config-${name}"],
+  }
+  
+  if str2bool($rm_build_folder) {
+    notice('remove build folder')
+
+    exec { "remove-${name}-build-folder":
+      cwd     => $src_cwd,
+      command => "rm -rf ${src_cwd}/${foldername}",
+      require => Exec["make-install-${name}"],
+    }
   }
 }
